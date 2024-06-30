efb8766b2116580a97167389c423b57734ff8463
MM20 <15646950+MM2-0@users.noreply.github.com>
2024-06-30 23:40:47 +0200

    Enable Proguard

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,13 +15,11 @@ android {
         targetSdk = 35
         versionCode = 1
         versionName = "1.0.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -41,7 +39,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.activitycompose)
